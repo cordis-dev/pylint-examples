824ca77f05f30f780ea738209089b4d9d5ece8bd
cordis-dev <cordis.dev@gmail.com>
2017-04-25 08:43:19 +0300

    Update literal_comparison.py

--- functional/literal_comparison.py
+++ functional/literal_comparison.py
@@ -40,8 +40,8 @@ if () is CONST:
 
 mystring = "ok"
 if mystring is "ok": # Returns true
-    # do stuff
+    pass
     
 mystring = "a" * 1000
 if mystring is ("a" * 1000): # This will return False
-    # do stuff
+    pass
